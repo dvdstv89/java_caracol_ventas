d964453395b7ada6fbd83a42920e9741829ad1a0
David <dvdstv89@gmail.com>
2023-11-26 16:45:39 -0500

    Salva inicial

---
+++ New folder/caracol-microservice-tienda/src/test/java/com/caracol/tienda/CaracolMicroserviceTiendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caracol.tienda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaracolMicroserviceTiendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
