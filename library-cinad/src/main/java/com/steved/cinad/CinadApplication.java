13179734fa7321bd9d318840b19b69d23b148ba9
David <dvdstv89@gmail.com>
2023-12-03 11:42:55 -0500

    Agregando libreria CINAD

---
+++ library-cinad/src/main/java/com/steved/cinad/CinadApplication.java
@@ -0,0 +1,13 @@
+package com.steved.cinad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinadApplication.class, args);
+	}
+
+}
